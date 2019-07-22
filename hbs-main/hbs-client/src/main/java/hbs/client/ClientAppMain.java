7d6a8cb01e0adfccc813652eba3c3c886a02d533
Kishore Veleti <kishore.veleti@NorthAdmins-MacBook-Air-2.local>
2019-07-22 19:19:12 -0400

    Initial version of Heart Beat Spring 3 solution

--- hbs-main/hbs-client/src/main/java/hbs/client/ClientAppMain.java
+++ hbs-main/hbs-client/src/main/java/hbs/client/ClientAppMain.java
@@ -3,7 +3,10 @@
  */
 package hbs.client;
 
+import java.util.Properties;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.mail.MailSender;
 
 import hbs.common.api.HeartBeatInfoService;
 
@@ -22,14 +25,31 @@ public class ClientAppMain {
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 		HeartBeatInfoService hbInfoServiceProxy = ctx.getBean(HeartBeatInfoService.class);
+		MailSender mailSender = ctx.getBean(MailSender.class);
 
 		GetServerHeartBeatScheduler heartBeatScheduler = ctx.getBean(GetServerHeartBeatScheduler.class);
-		heartBeatScheduler.setHeartBeatInfoService(hbInfoServiceProxy);
 
+		heartBeatScheduler.setHeartBeatInfoService(hbInfoServiceProxy);
+		heartBeatScheduler.setMailSender(mailSender);
+
+		Properties props = ctx.getBean("applicationProps", Properties.class);
+		populateEmailConfigFromProperties(heartBeatScheduler, props);
 		/*
 		 * Check out applicationContext.xml and application.properties files for
 		 * scheduler configuration
 		 */
+	}
+
+	private static void populateEmailConfigFromProperties(GetServerHeartBeatScheduler heartBeatScheduler,
+			Properties props) {
+
+		heartBeatScheduler.setSendEmailWhenServerRespondFailed(
+				props.getProperty("send.emai.when.server.heartbeat.error", "false").equalsIgnoreCase("true"));
+
+		heartBeatScheduler.setEmailTo(props.getProperty("mail.to"));
+		heartBeatScheduler.setEmailFrom(props.getProperty("mail.from"));
+		heartBeatScheduler.setEmailSubject(props.getProperty("mail.subject"));
+		heartBeatScheduler.setEmailMessage(props.getProperty("mail.message"));
 	}
 
 }
--- hbs-main/hbs-client/src/main/java/hbs/client/GetServerHeartBeatScheduler.java
+++ hbs-main/hbs-client/src/main/java/hbs/client/GetServerHeartBeatScheduler.java
@@ -3,12 +3,10 @@
  */
 package hbs.client;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.client.RestOperations;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 
 import hbs.common.api.HeartBeatInfoService;
 
@@ -24,6 +22,13 @@ public class GetServerHeartBeatScheduler {
 
 	private HeartBeatInfoService heartBeatInfoService;
 	private String serverURL;
+	private MailSender mailSender;
+	private boolean sendEmailWhenServerRespondFailed;
+
+	private String emailFrom;
+	private String emailTo;
+	private String emailSubject;
+	private String emailMessage;
 
 	public GetServerHeartBeatScheduler(String serverURL) {
 		this.serverURL = serverURL;
@@ -37,6 +42,56 @@ public class GetServerHeartBeatScheduler {
 		this.heartBeatInfoService = heartBeatInfoService;
 	}
 
+	public MailSender getMailSender() {
+		return mailSender;
+	}
+
+	public void setMailSender(MailSender mailSender) {
+		this.mailSender = mailSender;
+	}
+
+	public boolean isSendEmailWhenServerRespondFailed() {
+		return sendEmailWhenServerRespondFailed;
+	}
+
+	public void setSendEmailWhenServerRespondFailed(boolean sendEmailWhenServerRespondFailed) {
+		this.sendEmailWhenServerRespondFailed = sendEmailWhenServerRespondFailed;
+	}
+
+	public String getEmailFrom() {
+		return emailFrom;
+	}
+
+	public void setEmailFrom(String emailFrom) {
+		this.emailFrom = emailFrom;
+	}
+
+	public String getEmailTo() {
+		return emailTo;
+	}
+
+	public String getEmailSubject() {
+		return emailSubject;
+	}
+
+	public void setEmailSubject(String emailSubject) {
+		this.emailSubject = emailSubject;
+	}
+
+	public void setEmailTo(String emailTo) {
+		this.emailTo = emailTo;
+	}
+
+	public String getEmailMessage() {
+		return emailMessage;
+	}
+
+	public void setEmailMessage(String emailMessage) {
+		this.emailMessage = emailMessage;
+	}
+
+	// Look into this example https://www.baeldung.com/spring-remoting-http-invoker
+	// for how this client program users Spring Remote API to invoke the server
 	/*
 	 * CAUTION : Any change to this method name needs to be updated even in
 	 * applicationContext.xml This method is invoked because of configuration in
@@ -48,13 +103,34 @@ public class GetServerHeartBeatScheduler {
 
 			LOGGER.debug("Fetching the HeartBeat info for the server " + serverURL);
 
-			// Look into this example https://www.baeldung.com/spring-remoting-http-invoker
-			// for how this client program users Spring Remote API to invoke the server
-			this.heartBeatInfoService.provideHeartBeatInfo();
+			String receivedMessage = this.heartBeatInfoService.provideHeartBeatInfo();
+
+			LOGGER.debug("Received message from the heartbeat info server as " + receivedMessage);
 
 		} catch (Throwable throwable) {
-			// LOGGER.error("Exception occured while invoking the server for its heartbeat
-			// info", throwable);
+
+			LOGGER.error("Exception occured while invoking the server for its heartbeat info", throwable);
+			sendEmailThatServerNotRunning();
+		}
+	}
+
+	/*
+	 * https://www.javatpoint.com/spring-java-mail-tutorial
+	 */
+	private void sendEmailThatServerNotRunning() {
+
+		if (this.sendEmailWhenServerRespondFailed) {
+
+			// creating message
+			SimpleMailMessage message = new SimpleMailMessage();
+			message.setFrom(this.emailFrom);
+			message.setTo(this.emailTo);
+			message.setSubject(this.emailSubject);
+			message.setText(this.emailMessage);
+
+			// sending message
+			mailSender.send(message);
+
 		}
 
 	}
