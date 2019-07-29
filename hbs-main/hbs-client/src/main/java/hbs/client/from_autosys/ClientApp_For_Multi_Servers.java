ed14993cf9685aafc6ef9cdb2d73610809a24a8d
javakishore-veleti <training.my.hobby@gmail.com>
2019-07-28 23:53:15 -0400

    Implemented MultiServers heart beat check using Autosys invocation (simulation to Autosys)

--- hbs-main/hbs-client/src/main/java/hbs/client/from_autosys/ClientApp_For_Multi_Servers.java
+++ hbs-main/hbs-client/src/main/java/hbs/client/from_autosys/ClientApp_For_Multi_Servers.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.remoting.httpinvoker.HttpInvokerProxyFactoryBean;
 
